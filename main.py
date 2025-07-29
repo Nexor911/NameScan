17fe8a2f3082936fb3a2bffd64bdc25c19b113b8
Nexor <EgorSmirnov421232112@proton.me>
2025-07-29 23:15:18 +0300

    Обновление функций
    увеличен список сайтов 323, добавлен headers и возможность сохранения в файл

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import requests
 from sites import urls
 
+headers = {
+    "User-Agent": "Mozilla/5.0 (VenturaCounty; Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.6533.89"
+}
+
+found_account = []
+
 username = input("Введите юзернейм: ").strip()
 
+if not username:
+    print("никнейм отсутствует")
+    input("press enter to exit...")
+    exit()
 
 def check_account(bazo_url, username):
     try:
         url = bazo_url + username
-        response = requests.get(url, timeout=1)
+        response = requests.get(url, headers=headers, timeout=5)
         if response.status_code == 200:
-            print(f"Акк зареган {url}")
+            found_account.append(url)
+            print(f"[+] Акк зареган {url}")
             return True
         elif response.status_code == 404:
             print(f"Акк не найден {url}")
@@ -22,3 +33,7 @@ def check_account(bazo_url, username):
 
 for url in urls:
     check_account(url, username)
+
+with open("result.txt", "w") as f:
+    for account in found_account:
+        f.write(account + "\n")
